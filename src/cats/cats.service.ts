470bc9b008da47dd2cce255d97000bb2d69b44ea
GeethikaGeorge <geethikajessy@gmail.com>
2025-07-10 12:47:52 +0530

    Added forwardRef functuntion for illustrating circular ref example on catsservice and commonservice

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -27,12 +27,44 @@
 
 // }
 
+//Injection scpe example
 
-import { Injectable, Scope } from '@nestjs/common';
+// import { Injectable, Scope } from '@nestjs/common';
+// import { Cat } from './interfaces/cat.interface';
+
+// @Injectable({ scope: Scope.REQUEST }) 
+// export class CatsService {
+//   private readonly cats: Cat[] = [];
+
+//   create(cat: Cat) {
+//     this.cats.push(cat);
+//   }
+
+//   async findAll(): Promise<Cat[]> {
+//     return new Promise(resolve => {
+//       setTimeout(() => {
+//         resolve([
+//           { id: '1', name: 'Totoro', age: '5', breed: 'Persian' },
+//           { id: '2', name: 'Mochi', age: '3', breed: 'Siamese' },
+//         ]);
+//       }, 1000);
+//     });
+//   }
+// }
+
+//Circular dependency  resolving example
+// src/cats/cats.service.ts
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
+import { CommonService } from 'src/common/commo.service';
 import { Cat } from './interfaces/cat.interface';
 
-@Injectable({ scope: Scope.REQUEST }) 
+@Injectable()
 export class CatsService {
+  constructor(
+    @Inject(forwardRef(() => CommonService))
+    private commonService: CommonService,
+  ) {}
+
   private readonly cats: Cat[] = [];
 
   create(cat: Cat) {
@@ -40,13 +72,8 @@ export class CatsService {
   }
 
   async findAll(): Promise<Cat[]> {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve([
-          { id: '1', name: 'Totoro', age: '5', breed: 'Persian' },
-          { id: '2', name: 'Mochi', age: '3', breed: 'Siamese' },
-        ]);
-      }, 1000);
-    });
+    console.log('Calling CommonService from CatsService...');
+    this.commonService.logCatInfo(); // Circular use
+    return this.cats;
   }
 }
---
+++ src/common/commo.service.ts
@@ -0,0 +1,16 @@
+
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
+import { CatsService } from '../cats/cats.service';
+
+@Injectable()
+export class CommonService {
+  constructor(
+    @Inject(forwardRef(() => CatsService))
+    private catsService: CatsService,
+  ) {}
+
+  logCatInfo() {
+    console.log('Calling CatsService from CommonService...');
+    this.catsService.findAll();
+  }
+}
