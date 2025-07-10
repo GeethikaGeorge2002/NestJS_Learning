306bd74be48c8a99db07ee0c9e61ff9416483350
GeethikaGeorge <geethikajessy@gmail.com>
2025-07-10 12:26:09 +0530

    Added scope request for catservice

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,8 +1,37 @@
 
-import { Injectable } from '@nestjs/common';
+// import { Injectable } from '@nestjs/common';
+// import { Cat } from './interfaces/cat.interface';
+
+// @Injectable()
+// export class CatsService {
+//   private readonly cats: Cat[] = [];
+
+//   create(cat: Cat) {
+//     this.cats.push(cat);
+//   }
+
+//   // findAll(): Cat[] {
+//   //   return this.cats;
+//   // }
+  
+// async findAll(): Promise<Cat[]> {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve([
+//         { id: '1', name: 'Totoro', age: '5', breed: 'Persian' },
+//         { id: '2', name: 'Mochi', age: '3', breed: 'Siamese' },
+//       ]);
+//     }, 1000);
+//   });
+// }
+
+// }
+
+
+import { Injectable, Scope } from '@nestjs/common';
 import { Cat } from './interfaces/cat.interface';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST }) 
 export class CatsService {
   private readonly cats: Cat[] = [];
 
@@ -10,20 +39,14 @@ export class CatsService {
     this.cats.push(cat);
   }
 
-  // findAll(): Cat[] {
-  //   return this.cats;
-  // }
-  
-async findAll(): Promise<Cat[]> {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve([
-        { id: '1', name: 'Totoro', age: '5', breed: 'Persian' },
-        { id: '2', name: 'Mochi', age: '3', breed: 'Siamese' },
-      ]);
-    }, 1000);
-  });
+  async findAll(): Promise<Cat[]> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          { id: '1', name: 'Totoro', age: '5', breed: 'Persian' },
+          { id: '2', name: 'Mochi', age: '3', breed: 'Siamese' },
+        ]);
+      }, 1000);
+    });
+  }
 }
-
-}
-
